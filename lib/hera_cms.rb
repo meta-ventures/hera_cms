f608950ce20c77afe164aa51205cf32f804fba48
Rayan Castro <rayancdc@gmail.com>
2020-12-01 11:32:45 +0400

    Add first HeraCms accessor

--- lib/hera_cms.rb
+++ lib/hera_cms.rb
@@ -3,6 +3,10 @@ require "hera_cms/version"
 module HeraCms
   class Error < StandardError; end
 
+  class << self
+    attr_accessor :s3_bucket
+  end
+
   def self.test
     return "Successful"
   end
