8ea11089ef157be57c4535b39bbf1593d6a3fd5a
Rayan Castro <rayancdc@gmail.com>
2021-02-01 15:46:42 +0400

    Update migrations and add useful model methods

--- app/models/hera_cms/application_record.rb
+++ app/models/hera_cms/application_record.rb
@@ -1,5 +1,25 @@
 module HeraCms
   class ApplicationRecord < ActiveRecord::Base
     self.abstract_class = true
+    def self.identify(identifier)
+      self.find_by(identifier: identifier)
+    end
+
+    def update_json
+      element = read_json
+      element["#{self.identifier}"] = self.as_json.without("id", "created_at", "updated_at")
+      save_json(element)
+    end
+
+    def read_json
+      JSON.parse(File.read(File.join(Rails.root, 'db', 'seed', "#{self.class}.json")))
+    end
+
+    def save_json(element)
+      filepath = File.join(Rails.root, 'db', 'seed', "#{self.class}.json")
+      File.open(filepath, 'wb') do |file|
+        file.write(JSON.generate(element))
+      end
+    end
   end
 end
