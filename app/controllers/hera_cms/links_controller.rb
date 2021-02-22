4d03a98ea66ae601707033755f523b0ba081a058
Rayan Castro <rayancdc@gmail.com>
2021-02-22 23:30:08 +0400

    Add link edit for inner text

--- app/controllers/hera_cms/links_controller.rb
+++ app/controllers/hera_cms/links_controller.rb
@@ -1,19 +1,7 @@
 require_dependency "hera_cms/application_controller"
-require 'json'
 
 module HeraCms
   class LinksController < ApplicationController
-    # layout "admin"
-
-    # def index
-    #   link_json = HeraCms::Link.read_json
-    #   @seed_link = JSON.pretty_generate(link_json)
-    #   @links = HeraCms::Link.all.order(created_at: :desc)
-    # end
-
-    # def edit
-    #   @link = HeraCms::Link.find(params[:id])
-    # end
 
     def update
       @link = HeraCms::Link.find(params[:id])
@@ -25,32 +13,10 @@ module HeraCms
       end
     end
 
-    # def new
-    #   @link = HeraCms::Link.new
-    # end
-
-    # def create
-    #   @link = HeraCms::Link.new(link_params)
-
-    #   @link[:translated_text] << translate_service.mount_translation(params[:link]["inner_text"])
-
-    #   if @HeraCms::link.save
-    #     redirect_to URI(request.referer).path
-    #   else
-    #     render :new
-    #   end
-    # end
-
-    # def destroy
-    #   @link = HeraCms::Link.find(params[:id])
-    #   @HeraCms::link.destroy
-    #   redirect_to URI(request.referer).path
-    # end
-
     private
 
     def link_params
-      params.require(:link).permit(:identifier, :classes, :style, :path)
+      params.require(:link).permit(:identifier, :classes, :style, :path, :inner_text)
     end
   end
 end
