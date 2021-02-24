9d7f195fb3120536c76c4b031fa756df529c820f
Rayan Castro <rayancdc@gmail.com>
2021-02-24 10:19:38 +0400

    Add active storage support for image model

--- app/models/hera_cms/image.rb
+++ app/models/hera_cms/image.rb
@@ -1,6 +1,10 @@
 module HeraCms
   class Image < ApplicationRecord
     validates :identifier, presence: true, uniqueness: true
-
+    if HeraCms.active_storage?
+      has_one_attached :upload
+    else
+      validates :url, presence: true
+    end
   end
 end
