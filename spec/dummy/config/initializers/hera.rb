f42ab76722625f710c5c1d3df9ff85e37c6406b1
Rayan Castro <rayancdc@gmail.com>
2021-02-24 10:21:46 +0400

    Set dummy active storage to false (not configured for development)

--- spec/dummy/config/initializers/hera.rb
+++ spec/dummy/config/initializers/hera.rb
@@ -1,6 +1,6 @@
 # see https://github.com/horta-tech/hera_cms for more info
 
 HeraCms.setup do |config|
-  config.image_upload = true
+  config.image_upload = false
   config.upload_service = :active_storage
 end
