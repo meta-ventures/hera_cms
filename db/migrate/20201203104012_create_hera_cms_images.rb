b91ad0715192077da96d7164dc3e4e1d76f38e35
Rayan Castro <rayancdc@gmail.com>
2021-02-24 12:18:22 +0400

    Remove upload column from images

--- db/migrate/20201203104012_create_hera_cms_images.rb
+++ db/migrate/20201203104012_create_hera_cms_images.rb
@@ -2,7 +2,6 @@ class CreateHeraCmsImages < ActiveRecord::Migration[6.0]
   def change
     create_table :hera_cms_images do |t|
       t.string :identifier
-      t.string :upload
       t.string :url
       t.string :classes, default: ""
       t.string :style, default: ""
